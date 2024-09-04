8997d39f15613349bae1e1e8c93765b8b6ea5fc0
reefahtasnia <tasniareefah@gmail.com>
2024-09-04 13:29:43 +0600

    table_final altered with new address type

--- table_final.sql
+++ table_final.sql
@@ -3,6 +3,13 @@ CREATE SEQUENCE user_id_seq
     INCREMENT BY 1
     NOMAXVALUE;
 
+CREATE OR REPLACE TYPE Address_Type AS OBJECT (
+    Street   VARCHAR2(100),
+    Region   VARCHAR2(50),
+    District VARCHAR2(50),
+    Country  VARCHAR2(50)
+);
+/
 CREATE TABLE Users (
     userid NUMBER DEFAULT user_id_seq.NEXTVAL NOT NULL,
     firstname VARCHAR(50) NOT NULL,
@@ -12,7 +19,7 @@ CREATE TABLE Users (
     date_of_birth DATE,
     blood_group VARCHAR(5),
     phone_number VARCHAR(16),
-    address VARCHAR(255),
+    address Address_Type,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT PK_USERID PRIMARY KEY (userid)
 );
@@ -21,10 +28,11 @@ CREATE TABLE Users (
 ALTER TABLE USERS 
 ADD CONSTRAINT CHK_PHONE CHECK (LENGTH(PHONE_NUMBER) BETWEEN 11 AND 16);
 
+
 CREATE TABLE Doctors (
     BMDC VARCHAR(100) NOT NULL,
     fullname VARCHAR2(255) NOT NULL,
-    email VARCHAR2(255) NOT NULL UNIQUE,
+    email VARCHAR2(255) NOT NULL,
     gender VARCHAR2(10),
     phone VARCHAR2(15),
     dept VARCHAR2(100),
@@ -39,11 +47,15 @@ CREATE TABLE Doctors (
 
 CREATE TABLE Passwords (
     userid NUMBER ,
-    BMDC VARCHAR(100),
     hashed_password VARCHAR(255),
+    BMDC VARCHAR(100),
     CONSTRAINT FK_USERID FOREIGN KEY (userid) REFERENCES Users(userid) ON DELETE CASCADE,
     CONSTRAINT FK_BMDC FOREIGN KEY (BMDC) REFERENCES Doctors(BMDC) ON DELETE CASCADE
 );
+
+/**
+ The above are the modifications done on user table. run it sequentially.
+ */
 
 CREATE TABLE Medical_History (
     user_id INT,
@@ -62,11 +74,11 @@ CREATE TABLE Medicine (
 );
 CREATE TABLE Appointment (
     appointment_id INT PRIMARY KEY,
-    BMDC VARCHAR(100),
+    BMDC_no INT,
     date DATE,
     time TIME,
     day VARCHAR(255),
-    FOREIGN KEY (BMDC) REFERENCES Doctors(BMDC_no)
+    FOREIGN KEY (BMDC_no) REFERENCES Doctor(BMDC_no)
 );
 CREATE TABLE Books (
     booking_id INT PRIMARY KEY,
